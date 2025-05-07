beabf258c126b0330dbca602232e2f85bf5422c5
watashone <balabas191@gmail.com>
2025-05-07 23:56:31 +0300

    Change api.py, dockerfile to debian based

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,13 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix")
+def matrix_mult():
+    a = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": (a @ b).tolist()
+    }
